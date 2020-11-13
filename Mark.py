38f7105714fb7a3e9b39ac1c446da59cbd89e783
GalacticPigeon <71193321+GalacticPigeon@users.noreply.github.com>
2020-11-13 13:33:45 -0500

    Update Mark.py

--- Mark.py
+++ Mark.py
@@ -84,7 +84,7 @@ cogs_dir = "cogs"
 if __name__ == '__main__':
     for extension in [f.replace('.py', '') for f in listdir(cogs_dir) if isfile(join(cogs_dir, f))]:
         try:
-            bot.load_extension(cogs_dir + "." + extension)
+            client.load_extension(cogs_dir + "." + extension)
         except (discord.ClientException, ModuleNotFoundError):
             print(f'Failed to load extension {extension}.')
             raise e
